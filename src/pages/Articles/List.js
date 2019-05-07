afa9ba256b93948635c08fc5e4e87bdbccc3213e
hfpp2012 <hfpp2012@gmail.com>
2019-05-07 16:32:44 +0800

    35_render

--- src/pages/Articles/List.js
+++ src/pages/Articles/List.js
@@ -3,56 +3,43 @@ import { Table, Card } from 'antd';
 import { connect } from 'dva';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
-@connect(props => {
-  console.log(props);
-})
+@connect(({ users }) => ({
+  users,
+}))
 class List extends PureComponent {
-  dataSource = [
-    {
-      key: '1',
-      name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号',
-    },
-    {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号',
-    },
-  ];
-
   columns = [
     {
-      title: '姓名',
-      dataIndex: 'name',
-      key: 'name',
+      title: '用户名',
+      dataIndex: 'username',
+      key: 'username',
     },
     {
-      title: '年龄',
-      dataIndex: 'age',
-      key: 'age',
+      title: '邮箱',
+      dataIndex: 'email',
+      key: 'email',
     },
     {
-      title: '住址',
-      dataIndex: 'address',
-      key: 'address',
+      title: '创建时间',
+      dataIndex: 'created_at',
+      key: 'created_at',
     },
   ];
 
   componentDidMount() {
     const { dispatch } = this.props;
-    console.log(this.props);
     dispatch({
       type: 'users/fetch',
     });
   }
 
   render() {
+    const {
+      data: { list },
+    } = this.props;
     return (
       <PageHeaderWrapper title="用户列表">
         <Card>
-          <Table dataSource={this.dataSource} columns={this.columns} />
+          <Table dataSource={list} columns={this.columns} />
         </Card>
       </PageHeaderWrapper>
     );
--- src/pages/Articles/models/users.js
+++ src/pages/Articles/models/users.js
@@ -6,7 +6,6 @@ export default {
   state: {
     data: {
       list: [],
-      pagination: {},
     },
   },
 
